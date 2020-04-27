abb3909a5526befb755d28fbe9c0d8e2d2d8f196
Manank Patni <manank321@gmail.com>
2020-04-27 23:42:41 +0530

    Move web3 function to web3services

--- frontend/src/components/Staking/Staking.jsx
+++ frontend/src/components/Staking/Staking.jsx
@@ -4,23 +4,10 @@ import { connect } from "react-redux";
 import { Button } from "components";
 // Services and redux action
 import { UserAction } from "actions";
-import { ApiService } from "services";
-import { loadConditionalTokensRepo } from "../../services/conditionalTokens/logic/ConditionalTokens";
-import { loadMarketMakersRepo } from "../../services/conditionalTokens/logic/MarketMakers";
-import BigNumber from "bignumber.js";
-import { getAccount } from "../../services/conditionalTokens/Web3Service";
-import {
-  getConditionId,
-  getPositionId,
-} from "../../services/conditionalTokens/utils/markets";
+
+import { getAccount, buyToken } from "../../services/conditionalTokens/Web3Service";
 
 const markets = require("../../services/conditionalTokens/config.local.json");
-
-const MarketStage = {
-  Running: 0,
-  Paused: 1,
-  Closed: 2,
-};
 
 class Staking extends Component {
   constructor(props) {
@@ -31,10 +18,7 @@ class Staking extends Component {
       buyToken1Loading: false,
       buyToken2Loading: false,
       amount: 5,
-      conditionalTokensRepo: null,
-      marketMakersRepo: null,
       account: null,
-      marketInfo: null,
     };
   }
   // Runs on every keystroke to update the React state
@@ -46,173 +30,23 @@ class Staking extends Component {
     });
   };
 
-  buyToken = async (pOption) => {
-    if (pOption === "player1") {
-      console.log("Entered player 1");
+  handleBuyToken = async () => {
 
-      await this.buyConditionalToken(0);
+    await buyToken(this.state.account, this.state.amount)
 
-      const { setUser } = this.props;
-      console.log(setUser);
+    const { setUser } = this.props;
+    console.log(setUser);
 
-      setUser({ staking_done: true });
-    } else {
-      console.log("Entered player 2");
-
-      await this.buyConditionalToken(1);
-
-      const { setUser } = this.props;
-      console.log(setUser);
-
-      setUser({ staking_done: true });
-    }
-  };
-
-  getMarketInfo = async () => {
-    return new Promise(async (resolve, reject) => {
-      //if (!process.env.REACT_APP_ORACLE_ADDRESS) return
-      const collateral = await this.state.marketMakersRepo.getCollateralToken();
-      const conditionId = getConditionId(
-        process.env.REACT_APP_ORACLE_ADDRESS,
-        markets.markets[0].questionId,
-        markets.markets[0].outcomes.length
-      );
-      const payoutDenominator = await this.state.conditionalTokensRepo.payoutDenominator(
-        conditionId
-      );
-
-      const outcomes = [];
-      for (
-        let outcomeIndex = 0;
-        outcomeIndex < markets.markets[0].outcomes.length;
-        outcomeIndex++
-      ) {
-        const indexSet = (outcomeIndex === 0
-          ? 1
-          : parseInt(Math.pow(10, outcomeIndex).toString(), 2)
-        ).toString();
-        const collectionId = await this.state.conditionalTokensRepo.getCollectionId(
-          `0x${"0".repeat(64)}`,
-          conditionId,
-          indexSet
-        );
-        const positionId = getPositionId(collateral.address, collectionId);
-        const probability = await this.state.marketMakersRepo.calcMarginalPrice(
-          outcomeIndex
-        );
-        const balance = await this.state.conditionalTokensRepo.balanceOf(
-          this.state.account,
-          positionId
-        );
-        const payoutNumerator = await this.state.conditionalTokensRepo.payoutNumerators(
-          conditionId,
-          outcomeIndex
-        );
-
-        const outcome = {
-          index: outcomeIndex,
-          title: markets.markets[0].outcomes[outcomeIndex].title,
-          probability: new BigNumber(probability)
-            .dividedBy(Math.pow(2, 64))
-            .multipliedBy(100)
-            .toFixed(2),
-          balance: new BigNumber(balance).dividedBy(
-            Math.pow(10, collateral.decimals)
-          ),
-          payoutNumerator: payoutNumerator,
-        };
-        outcomes.push(outcome);
-      }
-
-      const marketData = {
-        lmsrAddress: markets.lmsrAddress,
-        title: markets.markets[0].title,
-        outcomes,
-        stage: MarketStage[await this.state.marketMakersRepo.stage()],
-        questionId: markets.markets[0].questionId,
-        conditionId: conditionId,
-        payoutDenominator: payoutDenominator,
-      };
-
-      this.setState({ marketInfo: marketData });
-      resolve();
-    });
-  };
-
-  buyConditionalToken = async (pSelectedOutcome) => {
-    await this.getMarketInfo();
-
-    console.log("marketinfo", this.state.marketInfo);
-
-    const collateral = await this.state.marketMakersRepo.getCollateralToken();
-    console.log("coolastrer", collateral);
-
-    const formattedAmount = new BigNumber(this.state.amount).multipliedBy(
-      new BigNumber(Math.pow(10, collateral.decimals))
-    );
-
-    const outcomeTokenAmounts = Array.from(
-      { length: this.state.marketInfo.outcomes.length },
-      (value, index) =>
-        index === pSelectedOutcome ? formattedAmount : new BigNumber(0)
-    );
-
-    const cost = await this.state.marketMakersRepo.calcNetCost(
-      outcomeTokenAmounts
-    );
-
-    console.log("cost", cost);
-
-    const collateralBalance = await collateral.contract.balanceOf(
-      this.state.account
-    );
-
-    console.log("collateralbal", collateralBalance);
-    console.log("graterornot", cost.gt(collateralBalance));
-
-    if (cost.gt(collateralBalance)) {
-      await collateral.contract.deposit({
-        value: formattedAmount.toString(),
-        from: this.state.account,
-      });
-      await collateral.contract.approve(
-        this.state.marketInfo.lmsrAddress,
-        formattedAmount.toString(),
-        {
-          from: this.state.account,
-        }
-      );
-    }
-
-    const tx = await this.state.marketMakersRepo.trade(
-      outcomeTokenAmounts,
-      cost,
-      this.state.account
-    );
-    console.log("trade transaction", { tx });
-  };
+    setUser({ staking_done: true });
+  }
 
   async componentDidMount() {
     console.log(process.env);
 
     let account = await getAccount();
-
     console.log("account is ", account);
 
-    let conditionalTokensRepo = await loadConditionalTokensRepo(
-      window.web3,
-      markets.lmsrAddress,
-      account[0]
-    );
-    let marketMakersRepo = await loadMarketMakersRepo(
-      window.web3,
-      markets.lmsrAddress,
-      account[0]
-    );
-
     this.setState({
-      conditionalTokensRepo: conditionalTokensRepo,
-      marketMakersRepo: marketMakersRepo,
       account: account[0],
     });
     this.isComponentMounted = true;
@@ -224,13 +58,13 @@ class Staking extends Component {
 
   render() {
     // Extract data from state
-    const { amount, buyToken1Loading, buyToken2Loading } = this.state;
+    const { amount, buyToken1Loading } = this.state;
     return (
       <div className="Staking">
         <div className="description">Who will win this game?</div>
         <div name="form">
           <div className="field">
-            <label>Stake Amount</label>
+            <label>Stake Amount on your Victory</label>
             <input
               type="number"
               name="username"
@@ -249,17 +83,9 @@ class Staking extends Component {
               type="submit"
               className="green"
               loading={buyToken1Loading}
-              onClick={(e) => this.buyToken("player1")}
+              onClick={(e) => this.handleBuyToken("player1")}
             >
-              {"You"}
-            </Button>
-            <Button
-              type="submit"
-              className="green"
-              loading={buyToken2Loading}
-              onClick={(e) => this.buyToken("player2")}
-            >
-              {"Opponent"}
+              {"Stake"}
             </Button>
           </div>
         </div>
--- frontend/src/services/conditionalTokens/Web3Service.js
+++ frontend/src/services/conditionalTokens/Web3Service.js
@@ -3,6 +3,171 @@ import LMSRMarketMaker from '../../contracts/build/LMSRMarketMaker.json'
 import WETH9 from '../../contracts/build/WETH9.json'
 import { default as Web3 } from 'web3';
 
+import {
+  getConditionId,
+  getPositionId,
+} from "./utils/markets";
+
+import BigNumber from "bignumber.js";
+
+import { loadConditionalTokensRepo } from "./logic/ConditionalTokens";
+import { loadMarketMakersRepo } from "./logic/MarketMakers";
+
+const markets = require("./config.local.json");
+
+const MarketStage = {
+  Running: 0,
+  Paused: 1,
+  Closed: 2,
+};
+
+let conditionalTokensRepo;
+let marketMakersRepo;
+let marketInfo;
+
+export const initialiseMarkets = async (account) => {
+  conditionalTokensRepo = await loadConditionalTokensRepo(
+    window.web3,
+    markets.lmsrAddress,
+    account
+  );
+
+  marketMakersRepo = await loadMarketMakersRepo(
+    window.web3,
+    markets.lmsrAddress,
+    account
+  );
+}
+
+export const buyToken = async (account, amount) => {
+  await initialiseMarkets(account)
+  console.log("Entered player 1");
+  await buyConditionalToken(0, account, amount);
+};
+
+export const buyConditionalToken = async (pSelectedOutcome, account, amount) => {
+  await getMarketInfo(account);
+
+  console.log("marketinfo", marketInfo);
+
+  const collateral = await marketMakersRepo.getCollateralToken();
+  console.log("coolastrer", collateral);
+
+  const formattedAmount = new BigNumber(amount).multipliedBy(
+    new BigNumber(Math.pow(10, collateral.decimals))
+  );
+
+  const outcomeTokenAmounts = Array.from(
+    { length: marketInfo.outcomes.length },
+    (value, index) =>
+      index === pSelectedOutcome ? formattedAmount : new BigNumber(0)
+  );
+
+  const cost = await marketMakersRepo.calcNetCost(
+    outcomeTokenAmounts
+  );
+
+  console.log("cost", cost);
+
+  const collateralBalance = await collateral.contract.balanceOf(
+    account
+  );
+
+  console.log("collateralbal", collateralBalance);
+  console.log("graterornot", cost.gt(collateralBalance));
+
+  if (cost.gt(collateralBalance)) {
+    await collateral.contract.deposit({
+      value: formattedAmount.toString(),
+      from: account,
+    });
+    await collateral.contract.approve(
+      marketInfo.lmsrAddress,
+      formattedAmount.toString(),
+      {
+        from: account,
+      }
+    );
+  }
+
+  const tx = await marketMakersRepo.trade(
+    outcomeTokenAmounts,
+    cost,
+    account
+  );
+  console.log("trade transaction", { tx });
+};
+
+export const getMarketInfo = async (account) => {
+  return new Promise(async (resolve, reject) => {
+    //if (!process.env.REACT_APP_ORACLE_ADDRESS) return
+    const collateral = await marketMakersRepo.getCollateralToken();
+    const conditionId = getConditionId(
+      process.env.REACT_APP_ORACLE_ADDRESS,
+      markets.markets[0].questionId,
+      markets.markets[0].outcomes.length
+    );
+    const payoutDenominator = await conditionalTokensRepo.payoutDenominator(
+      conditionId
+    );
+
+    const outcomes = [];
+    for (
+      let outcomeIndex = 0;
+      outcomeIndex < markets.markets[0].outcomes.length;
+      outcomeIndex++
+    ) {
+      const indexSet = (outcomeIndex === 0
+        ? 1
+        : parseInt(Math.pow(10, outcomeIndex).toString(), 2)
+      ).toString();
+      const collectionId = await conditionalTokensRepo.getCollectionId(
+        `0x${"0".repeat(64)}`,
+        conditionId,
+        indexSet
+      );
+      const positionId = getPositionId(collateral.address, collectionId);
+      const probability = await marketMakersRepo.calcMarginalPrice(
+        outcomeIndex
+      );
+      const balance = await conditionalTokensRepo.balanceOf(
+        account,
+        positionId
+      );
+      const payoutNumerator = await conditionalTokensRepo.payoutNumerators(
+        conditionId,
+        outcomeIndex
+      );
+
+      const outcome = {
+        index: outcomeIndex,
+        title: markets.markets[0].outcomes[outcomeIndex].title,
+        probability: new BigNumber(probability)
+          .dividedBy(Math.pow(2, 64))
+          .multipliedBy(100)
+          .toFixed(2),
+        balance: new BigNumber(balance).dividedBy(
+          Math.pow(10, collateral.decimals)
+        ),
+        payoutNumerator: payoutNumerator,
+      };
+      outcomes.push(outcome);
+    }
+
+    const marketData = {
+      lmsrAddress: markets.lmsrAddress,
+      title: markets.markets[0].title,
+      outcomes,
+      stage: MarketStage[await marketMakersRepo.stage()],
+      questionId: markets.markets[0].questionId,
+      conditionId: conditionId,
+      payoutDenominator: payoutDenominator,
+    };
+
+    marketInfo = marketData
+    resolve();
+  });
+};
 
 const TruffleContract = require('@truffle/contract')
 
@@ -33,6 +198,8 @@ export const getAccount = async () => {
     }
     return account;
 }
+
+
 
 const resetContracts = () => {
   contracts = undefined
